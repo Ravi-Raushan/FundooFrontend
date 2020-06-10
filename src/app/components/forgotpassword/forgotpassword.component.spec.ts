ce60f5ccaf95823278e993284bd7c8df065bcb82
Ravi-Raushan <krravi015@gmail.com>
2020-06-10 09:00:17 +0530

    [Ravi Raushan]Added test cases for forgotPassword component

--- src/app/components/forgotpassword/forgotpassword.component.spec.ts
+++ src/app/components/forgotpassword/forgotpassword.component.spec.ts
@@ -1,17 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ForgotpasswordComponent } from './forgotpassword.component';
-import { MatCardModule} from '@angular/material';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { UserService } from 'src/app/service/userservice.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-describe('ForgotpasswordComponent', () => {
+fdescribe('ForgotpasswordComponent', () => {
   let component: ForgotpasswordComponent;
   let fixture: ComponentFixture<ForgotpasswordComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ForgotpasswordComponent,MatFormFieldModule ],
-      imports:[MatCardModule]
+      imports: [RouterTestingModule,FormsModule,ReactiveFormsModule,HttpClientTestingModule],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+      declarations: [ ForgotpasswordComponent]
+    
     })
     .compileComponents();
   }));
@@ -24,5 +31,19 @@ describe('ForgotpasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Initial value of email id should empty', () => {
+    expect(component.emailFormControl.value).toEqual('');
+  });
+
+  it('email id should not null', () => {
+    component.emailFormControl.setValue(null);
+    expect(component.getEmailErrorMessage()).toEqual("Email id is required");
+  });
+
+  it('email should be valid', () => {
+    component.emailFormControl.setValue("xy.com");
+    expect(component.getEmailErrorMessage()).toEqual("Please enter a valid email id");
   });
 });
--- src/app/components/forgotpassword/forgotpassword.component.ts
+++ src/app/components/forgotpassword/forgotpassword.component.ts
@@ -1,27 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import {
   FormControl,
-  FormGroupDirective,
-  NgForm,
   Validators
 } from "@angular/forms";
-import { ErrorStateMatcher } from "@angular/material/core";
 import { Router } from "@angular/router";
 import { UserService } from "../../service/userservice.service";
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(
-      control &&
-      control.invalid &&
-      (control.dirty || control.touched || isSubmitted)
-    );
-  }
-}
 
 @Component({
   selector: "app-forgotpassword",
@@ -73,5 +56,5 @@ export class ForgotpasswordComponent implements OnInit {
   login() {
     this.router.navigate(["login"]);
   }
-  matcher = new MyErrorStateMatcher();
+  
 }
